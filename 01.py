ec7ba70f6675fbfd826091dc801742fee10e7c16
Mayank Jaiswal <133474498+jmayank2004@users.noreply.github.com>
2026-01-13 17:53:38 +0530

    13Jan2026

---
+++ 01.py
@@ -0,0 +1,7 @@
+print("Hello")
+
+x=input("Enter your age=")
+if(int(x)>18):
+    print("you are eligible to vote.")
+else:
+   print("you are not eligible to vote.")
---
+++ Built_in_Functions.py
@@ -0,0 +1,5 @@
+import math
+print(math.sqrt(64))
+print(math.log(2))
+print(math.gamma(10))
+print(math.pi)
