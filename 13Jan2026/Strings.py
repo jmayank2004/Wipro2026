665cac7e8ab12b8c4dae973da41ce41596ebe3ca
Mayank Jaiswal <133474498+jmayank2004@users.noreply.github.com>
2026-01-14 17:44:34 +0530

    13jan2026

---
+++ 13Jan2026/Strings.py
@@ -0,0 +1,23 @@
+a="Hello "
+print(a[0])
+print(a[-3])
+b="""My name is
+mayank"""
+print(b)
+print(a[2:])
+print(b[4:10])
+print(a  + b)
+
+c="aalfjslfkajlbhsbdjkn"
+print(c.upper())
+print(b.replace("mayank","shubham"))
+
+print("mayank" not in b )
+print("mayank" in b)
+print(b)
+
+d="My name is {0} and I am from {1}" .format("mayank","bhopal")
+print(d)
+
+e=input("Enter your name = ").split()
+print(e)
